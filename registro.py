d834629f254d46cff23e264bc535b0b8b8e1c4aa
juanp220077-tech <juanp220077@gmail.com>
2026-05-17 11:10:44 -0500

    se modifica algunoa parametroa

--- registro.py
+++ registro.py
@@ -1,14 +1,14 @@
 import sqlite3
 
 class Estudiante:
-	def __init__(self, nombre, carrera):
+	def __init__(self,nombre, carrera):
 		self.nombre = nombre
 		self.carrera = carrera
 	def guardar(self):
-		conn = sqlite3.connect('ubolivar.db')
+		conn = sqlite3.connect('Audla.db')
 		cursor = conn.cursor()
-		cursor.execute(" CREATE TABLE IF NOT EXISTS alumnos (nombre TEXT, carrera TEXT)")
+		cursor.execute("CREATE TABLE IF NOT EXISTS alumnos (nombre TEXT, carrera TEXT)")
 		cursor.execute("INSERT INTO alumnos VALUES (?,?)",(self.nombre, self.carrera))
-		print(f"Regisyro de: {self.nombre} guardado exitosamente.")
-mi_alumno = Estudiante("Juan Puente","Informatica")
-mi_alumno.guardar()
+		print(f"Registro de : {self.nombre} registrado com exito")
+alumno_nuevo = Estudiante("Amir Puente","desarollo de software")
+alumno_nuevo.guardar()
