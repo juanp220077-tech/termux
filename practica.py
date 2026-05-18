a342a36b933cf2b84e1410425917c38c7c369723
juanp220077-tech <juanp220077@gmail.com>
2026-05-18 18:15:20 -0500

    con el.awhunso cambio

--- practica.py
+++ practica.py
@@ -5,13 +5,14 @@ class Persona:
 		self.nombre = nombre
 		self.__id_interno = "ID-999"
 	def saludar(self):
-		print(f"hola soy {self.nombre}")
+		print(f"Hola soy {self.nombre}")
 class Estudiante(Persona):
-	def __init__(self,nombre, carrera):
+	def __init__(self,nombre,carrera):
 		super().__init__(nombre)
 		self.carrera = carrera
 	def mostrar_detalle(self):
 		print(f"{self.nombre} estudia {self.carrera}")
-alumno = Estudiante("Martin Puente", "diswño grafico")
+
+alumno = Estudiante("Juan puent","Telecomumicacionws")
 alumno.saludar()
 alumno.mostrar_detalle()
